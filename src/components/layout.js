1887c2fc1383a5648e2eb94b5d85ce5cf8c4572e
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-04-24 23:21:23 -0400

    layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,32 +13,12 @@ import Header from "./header"
 import "./layout.scss"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+      <div>
+        <p>DENNIS</p>
       </div>
     </>
   )
